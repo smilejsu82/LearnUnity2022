81bfad064f1834a78063fdbae5c31497ec7e29f4
OSK <blackstar090977@gmail.com>
2022-01-18 18:07:25 +0900

    중간저장3

--- CatEscape/CatEscape/Assets/Scripts/GameDirector.cs
+++ CatEscape/CatEscape/Assets/Scripts/GameDirector.cs
@@ -6,14 +6,38 @@ using UnityEngine.UI;
 public class GameDirector : MonoBehaviour
 {
     GameObject hpGauge;
+    GameObject player;
+    GameObject score;
+    private float point;
+    GameObject timer;
+    public float time;
+    private float decreaseTime = 1.0f;
+    private float delta = 0;
     // Start is called before the first frame update
     void Start()
     {
         this.hpGauge = GameObject.Find("hpGauge");
+        this.player = GameObject.Find("player");
+        this.score = GameObject.Find("score");
+        this.timer = GameObject.Find("timer");
+    }
+
+    void Update()
+    {
+        delta += Time.deltaTime;
+        if (this.delta > this.decreaseTime)
+        {
+            this.delta = 0;
+            time -= decreaseTime;
+        }
+        this.score.GetComponent<Text>().text = "현재 점수:" + point + "점";
+        this.timer.GetComponent<Text>().text = "남은시간:" + time + "초";
+
     }
 
     public void DecreaseHp()
     {
+        this.player.GetComponent<PlayerController>().Hp -= 1;
         this.hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
     }
 }
--- CatEscape/CatEscape/Assets/Scripts/PlayerController.cs
+++ CatEscape/CatEscape/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Hp;
+
     // Start is called before the first frame update
     void Start()
     {
